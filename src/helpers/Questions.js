db5aef62eea6eae1c3064e67019a3a21ba0b1287
Pragati Prasanna <pragatiprasanna@Pragatis-MacBook-Pro.local>
2024-03-09 21:29:21 +0530

    initial commit

--- src/helpers/Questions.js
+++ src/helpers/Questions.js
@@ -1,204 +1,103 @@
 export const Questions = [
     {
-        question: "What Is The Name Of The Secret Group Helping Whiterose?",
-        optionA: "Dark Army",
-        optionB: "Death Squad",
-        optionC: "Dark Order",
-        optionD: "Army of Darkness",
-        answer: "a",
-        hint: "Friend. If you don't know the most popular hacker group, what do you know?",
-        img: "https://c1.staticflickr.com/9/8270/29962849562_5db94879c5_b.jpg"
-    },
-    {
-        question: "Where was Whiterose trying to get her project shipped to?",
-        optionA: "Democratic Republic of the Congo",
-        optionB: "China",
-        optionC: "United Arab Emirates",
-        optionD: "New Zealand",
-        answer: "a",
-        hint: "+243. I know that's enough.",
-        img: "https://s24195.pcdn.co/wp-content/uploads/2017/12/Cover-photo-Mr-Robot.jpg"
-    },
-    {
-        question: "Why was the Mr Robot personality created?",
-        optionA: "Elliot's father sexually molested him",
-        optionB: "Elliot's father never existed",
-        optionC: "Elliot's father beat him",
-        optionD: "Elliot's father hated him",
-        answer: "a",
-        hint: "No hints for easy questions.",
-        img: "https://variety.com/wp-content/uploads/2015/08/mr-robot-finale-tv-review-usa.jpg?w=1000"
-    },
-    {
-        question: "'Isn't it obvious? You're not Elliot. You're the M-'",
-        optionA: "Mastermind",
-        optionB: "Monster",
-        optionC: "Mistake",
-        optionD: "Mentor",
-        answer: "a",
-        hint: "Do you like board games?",
-        img: "https://www.seriangolo.it/wp-content/uploads/2015/07/vlcsnap-2015-07-22-11h01m59s155.jpg"
-    },
-    {
-        question: "Where was Whiterose's project located?",
-        optionA: "Under the Washington Township power plant",
-        optionB: "In an underground facility in China",
-        optionC: "In a secretive facility in the Congo",
-        optionD: "At a CERN facility in Switzerland",
-        answer: "a",
-        hint: "This place caused Elliot’s and his deceased best friend/secret love of his life, Angela Moss’s, parents to die of cancer.",
-        img: "https://i2.wp.com/timetravelnexus.com/wp-content/uploads/2019/12/Dec-2019-MrRobot-WashTownship.png"
-    },
-    {
-        question: "Who was in the room with Angela before Whiterose came in?",
-        optionA: "Her mother",
-        optionB: "Her younger self",
-        optionC: "Her younger sister",
-        optionD: "A child Elliot",
+        question: "Who is the author of the Sherlock Holmes stories?",
+        optionA: "Agatha Christie",
+        optionB: "Arthur Conan Doyle",
+        optionC: "Edgar Allan Poe",
+        optionD: "Dashiell Hammett",
         answer: "b",
-        hint: "Do you believe in impossible?",
-        img: "https://preview.redd.it/s5ssj1uimbrz.png?auto=webp&s=0c95a39b6310a785f203c60334ab9b3c4e745f0d"
+        hint: "Friend. If you don't know Who is the author of the Sherlock Holmes stories, what do you know?",
+        img: "https://media.istockphoto.com/id/1474525598/photo/arthur-conan-doyle-a-portrait-from-scottish-money.webp?b=1&s=170667a&w=0&k=20&c=Rg3r3_S-haGIhlT94DQrvPDtM-icecjLZmFVOUU1inc="
     },
     {
-        question: "Why did Elliot fall out of the window?",
-        optionA: "His father was angry at him and pushed him",
-        optionB: "Mr Robot took control and jumped to escape his father",
-        optionC: "He saw Mr Robot and thought he wanted to hurt him",
-        optionD: "He tripped while trying to escape his mother",
+        question: "Which famous phrase did Sherlock Holmes use to describe his approach to solving crimes?",
+        optionA: "Deductive reasoning",
+        optionB: "Occam's Razor",
+        optionC: "Inference to the best explanation",
+        optionD: "The end justifies the means",
+        answer: "a",
+        hint: "Sherlock Holmes' investigative method relies heavily on logic, observation, and this type of reasoning. I know that's enough.",
+        img: "https://images.unsplash.com/photo-1585372237501-8bd95efa1bcd?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2hlcmxvY2slMjBob2xtZXN8ZW58MHx8MHx8fDA%3D"
+    },
+    {
+        question: "What is Sherlock Holmes' favorite phrase when he solves a case?",
+        optionA: "Elementary, my dear Watson.",
+        optionB: "The game is afoot.",
+        optionC: "It's a mystery.",
+        optionD: "Eureka!",
+        answer: "a",
+        hint: "This iconic phrase is often attributed to Sherlock Holmes and is used when he explains a seemingly simple solution to a complex problem.",
+        img: "https://images.unsplash.com/photo-1594024561977-4774ad2e6eae?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2hlcmxvY2slMjBob2xtZXN8ZW58MHx8MHx8fDA%3D"
+    },
+    {
+        question: "Who is Sherlock Holmes' arch-nemesis?",
+        optionA: "Dr. John Watson",
+        optionB: "James Moriarty",
+        optionC: "Mycroft Holmes",
+        optionD: "Irene Adler",
         answer: "b",
+        hint: "No hints for easy questions.?",
+        img: "https://miro.medium.com/v2/resize:fit:1400/1*ID70VooLDHGpmWtpbCkUKg.png"
+    },
+    {
+        question: "What is the title of the first Sherlock Holmes novel written by Arthur Conan Doyle?",
+        optionA: "The Hound of the Baskervilles",
+        optionB: "A Study in Scarlet",
+        optionC: "The Sign of Four",
+        optionD: "The Adventures of Sherlock Holmes",
+        answer: "b",
+        hint: "This novel marked the debut of Sherlock Holmes and Dr. John Watson as detective partners.",
+        img: "https://m.media-amazon.com/images/I/5123LKYlYzL.jpg"
+    },
+    {
+        question: "What is the address of Sherlock Holmes' residence in London?",
+        optionA: "221B Baker Street",
+        optionB: "10 Downing Street",
+        optionC: "221C Baker Street",
+        optionD: "10 Baker Street",
+        answer: "a",
+        hint: "The address of Sherlock Holmes' residence is a famous London landmark known for its association with the detective.",
+        img: "https://cdn.yopacrm.yopa.co.uk/homeowners-hub/uploads/2022/07/221b-baker-street-door-626x1024.jpg"
+    },
+    {
+        question: "Who is Sherlock Holmes' trusted friend and companion?",
+        optionA: "John Watson",
+        optionB: "James Moriarty",
+        optionC: "Mycroft Holmes",
+        optionD: "Irene Adler",
+        answer: "a",
         hint: "Friend. This is pretty easy question. I would be ashamed if you need help.",
-        img: "https://i.imgur.com/hCktCz8.jpeg"
+        img: "https://media.sciencephoto.com/c0/07/24/76/c0072476-800px-wm.jpg"
     },
     {
-        question: "What was the biggest consequence of the 5/9 hack?",
-        optionA: "A war erupted in Saudi Arabia",
-        optionB: "E Coin became the global currency",
-        optionC: "3 separate terrorist attacks in Washington D.C.",
-        optionD: "USA lost its footing as a global superpower",
-        answer: "b",
-        hint: "What do you think about cryptocurrencies?",
-        img: "https://i.vimeocdn.com/video/654926144_1280x720.jpg"
+        question: "What is the name of Sherlock Holmes' landlady at 221B Baker Street who frequently worries about his health and habits?",
+        optionA: "Mrs. Hudson",
+        optionB: "Mrs. Watson",
+        optionC: "Mrs. Morstan",
+        optionD: "Mrs. Smith",
+        answer: "a",
+        hint: " She is often depicted as a caring and motherly figure to Sherlock Holmes and Dr. John Watson.",
+        img: "https://pbs.twimg.com/media/C1rbbiBXgAA-2p_.jpg"
     },
     {
-        question: "Where would Angela and Elliot runaway to as kids?",
-        optionA: "Empire State Building",
-        optionB: "Queens Museum",
-        optionC: "Central Park",
-        optionD: "Coney Island",
-        answer: "b",
-        hint: "You should inspect this more.",
-        img: "https://i.pinimg.com/originals/7c/3b/47/7c3b476d41ee54cf320df9990453c807.jpg"
+        question: "What is the name of Sherlock Holmes' older brother, who is depicted as possessing even greater powers of deduction?",
+        optionA: "Mycroft Holmes",
+        optionB: "John Watson",
+        optionC: "James Moriarty",
+        optionD: "Inspector Lestrade",
+        answer: "a",
+        hint: "This character holds a high-ranking government position and is often consulted by Sherlock Holmes for his expertise.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Gree-02.jpg/640px-Gree-02.jpg"
     },
     {
-        question: "Which rapper has guest starred on the show?",
-        optionA: "A$AP Rocky",
-        optionB: "Joey Badass",
-        optionC: "Kendrick Lamar",
-        optionD: "Benny The Butcher",
-        answer: "b",
-        hint: "'Y'all went to high school, I went to school high.'",
-        img: "https://i.pinimg.com/originals/f0/70/e6/f070e6aa387fe25ca4b1936c197f7714.jpg"
-    },
-    {
-        question: "What were Terry Colby and the other E Corp executives eating while deciding on how to handle the Washington Township Scandal?",
-        optionA: "Caviar",
-        optionB: "Lobster thermidor",
-        optionC: "Shrimp cocktail",
-        optionD: "Vegetable carpaccio",
+        question: "In which collection of Sherlock Holmes stories does the tale 'The Adventure of the Speckled Band' appear?",
+        optionA: "The Memoirs of Sherlock Holmes",
+        optionB: "The Return of Sherlock Holmes",
+        optionC: "The Adventures of Sherlock Holmes",
+        optionD: "His Last Bow",
         answer: "c",
-        hint: "Friend. Do you like to cook? You will need 10 glasses of cold water, 2 medium carrots, 2 stalks celery, 1 large onion...",
-        img: "https://wallpapercave.com/wp/wp4507684.jpg"
-    },
-    {
-        question: "What was the name of the pyro that Elliot was fascinated with in season 2?",
-        optionA: "Hot Gina",
-        optionB: "Hot Stacey",
-        optionC: "Hot Carla",
-        optionD: "Hot Laurel",
-        answer: "c",
-        hint: "'At 2 p.m., we check out a basketball game. Hot _____, the local pyro, is usually there. She's become like my personal totem.'",
-        img: "https://i.imgur.com/6wc6bC1.jpg"
-    },
-    {
-        question: "What was Elliot's favorite candy?",
-        optionA: "Smarties",
-        optionB: "Tic Tacs",
-        optionC: "Sour Patch Kids",
-        optionD: "Reese's Cups",
-        answer: "c",
-        hint: "I don't know why, but they are very reminiscent of Groot. Do you think the same?",
-        img: "https://i1.wp.com/junkbanter.com/wp-content/uploads/2020/05/IMG_5789-scaled.jpg"
-    },
-    {
-        question: "What kind of car did Leon drive in season 3?",
-        optionA: "Mustang",
-        optionB: "Impala",
-        optionC: "Cadillac",
-        optionD: "Bentley",
-        answer: "c",
-        hint: "These guys have famous songs in which this car appears: Bruno Mars, Train, Roseanne Cash, Karen Lawrence, Mink DeVille... ",
-        img: "https://external-preview.redd.it/0nhTPXW_fEsSday-8VqEYTbKukNoy3RxKGGFxffAyF0.png?auto=webp&s=e0f7be349f78928746514dfe28d38acc3a0429b2"
-    },
-    {
-        question: "What was the name of Irving's novel?",
-        optionA: "Careful, Friend",
-        optionB: "A Wink Gone Wrong",
-        optionC: "Beach Towel",
-        optionD: "A Rose, So Sickly",
-        answer: "c",
-        hint: "'A story could have a mediocre beginning and middle, and oftentimes it does, but always gotta have a wow ending. Otherwise, what's the point?'",
-        img: "https://preview.redd.it/6aoq9jt72k341.jpg?width=1792&format=pjpg&auto=webp&s=d555761f4c63b72e05e01419d6d3e2ab0ea7f6ba"
-    },
-    {
-        question: "What did the fsociety.dat file say?",
-        optionA: "DON'T TELL ANYONE",
-        optionB: "KEEP THIS SAFE",
-        optionC: "DON'T DELETE ME",
-        optionD: "LEAVE ME HERE",
-        answer: "d",
-        hint: "Are you looking in the right places?",
-        img: "https://preview.redd.it/0zw17rx7z1541.jpg?auto=webp&s=31eac5c8e060c596ba0fdd3944857a7145b6894c"
-    },
-    {
-        question: "Who saved Dom's family from the Dark Army?",
-        optionA: "Shane Bernagh",
-        optionB: "Charles Bowden",
-        optionC: "Oliver Byrne",
-        optionD: "Deegan Maguire",
-        answer: "d",
-        hint: "If you need someone to get you a new ID and to make you disappear, you should call this man.",
-        img: "https://preview.redd.it/cdx5ayl1fhs31.png?width=682&format=png&auto=webp&s=f799fcf6b9cd47def11f7a605459729209aad8f7"
-    },
-    {
-        question: "What position does Zhi Zhang hold in the Chinese Government?",
-        optionA: "Minister of Public Security",
-        optionB: "Minister of Finance",
-        optionC: "Minister of Foreign Affairs",
-        optionD: "Minister of State Security",
-        answer: "d",
-        hint: "Guoanbu. It is enough.",
-        img: "https://decider.com/wp-content/uploads/2019/10/mr-robot-forbidden-bd-wong.jpg?quality=80&strip=all"
-    },
-    {
-        question: "What drug did Elliot overdose on in season 2?",
-        optionA: "Morphine",
-        optionB: "Heroin",
-        optionC: "Suboxone",
-        optionD: "Adderall",
-        answer: "d",
-        hint: "Drugs are bad.",
-        img: "https://thecomeback.com/wp-content/uploads/2016/07/elliot_ep2.jpg",
-        
-    },
-    {
-        question: "What music video is playing on the TV in the flashback with Whiterose and her lover?",
-        optionA: "Time Passages - Al Stewart",
-        optionB: "Too Much Time On My Hands - Styx",
-        optionC: "If I Could Turn Back Time - Cher",
-        optionD: "Time (Clock of the Heart) - Creature Club",
-        answer: "d",
-        hint: "this is a Difficult one. but you shoulD think a little more. Definitely.",
-        img: "https://preview.redd.it/wjmbgfd9cmu31.png?auto=webp&s=9bf96f0e6d62eb5a55b123076011fbb4a77b65c7"
+        hint: "This collection includes various short stories featuring Sherlock Holmes and Dr. John Watson's adventures.",
+        img: "https://cdn.kobo.com/book-images/7fc19695-0852-4b12-a286-c7dc122536e2/1200/1200/False/the-great-adventures-of-sherlock-holmes-1.jpg"
     },
 
 ]
